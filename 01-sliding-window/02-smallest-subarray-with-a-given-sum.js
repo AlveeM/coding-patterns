bb56c2f1443e645f51848573484cb06bcf50a7e7
Alvee Akand <AlveeM@users.noreply.github.com>
2020-01-13 21:52:50 -0500

    added smallest subarray with a given sum

---
+++ 01-sliding-window/02-smallest-subarray-with-a-given-sum.js
@@ -0,0 +1,23 @@
+const smallest_subarray_with_given_sum = function(s, arr) {
+    let minLength = Infinity;
+    let windowSum = 0;
+    let windowStart = 0;
+
+    for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+        windowSum += arr[windowEnd];
+        while (windowSum >= s) {
+            minLength = Math.min(minLength,(windowEnd - windowStart + 1));
+            windowSum -= arr[windowStart];
+            windowStart += 1;
+        }
+    }
+
+    if (minLength === Infinity) {
+        return 0;
+    }
+    return minLength;
+};
+
+console.log(`Smallest subarray length: ${smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 3, 2])}`); // 2
+console.log(`Smallest subarray length: ${smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8])}`); // 1
+console.log(`Smallest subarray length: ${smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6])}`); // 3
