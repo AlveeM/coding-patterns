262bf2837a49735712327edfd2028784049ff516
Alvee Akand <AlveeM@users.noreply.github.com>
2020-02-11 16:49:41 -0500

    added fruits-into-baskets in sliding window

---
+++ 01-sliding-window/04-fruits-into-baskets.js
@@ -0,0 +1,27 @@
+const fruits_into_baskets = fruits => {
+    let windowStart = 0;
+    let maxLength = 0;
+    let fruitFrequency = {};
+
+    for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
+        const rightFruit = fruits[windowEnd];;
+        if (!(rightFruit in fruitFrequency)) {
+            fruitFrequency[rightFruit] = 0;
+        }
+        fruitFrequency[rightFruit] += 1;
+
+        while (Object.keys(fruitFrequency).length > 2) {
+            const leftFruit = fruits[windowStart];
+            fruitFrequency[leftFruit] -= 1;
+            if (fruitFrequency[leftFruit] === 0) {
+                delete fruitFrequency[leftFruit];
+            }
+            windowStart += 1;
+        }
+        maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    }
+    return maxLength;
+}
+
+console.log(`Maximum number of fruits: ${fruits_into_baskets(['A', 'B', 'C', 'A', 'C'])}`);
+console.log(`Maximum number of fruits: ${fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C'])}`);
